4c5ec7f62789811398742697c9666f42400a0541
Jonas Gloning <34194370+jonasgloning@users.noreply.github.com>
2022-05-25 10:11:40 +0200

    update PeerJS version

--- ejs.js
+++ ejs.js
@@ -1,7 +1,7 @@
 const ejs = require("ejs");
 const fs = require("fs");
 
-const PEERJS_VERSION = "1.3.2";
+const PEERJS_VERSION = "1.4.5";
 
 function render(file, data) {
   ejs.renderFile("./views/" + file + ".ejs", data, {}, (err, str) => {
